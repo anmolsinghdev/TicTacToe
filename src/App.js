deb495fd741ff051934bea357b792ab09bb2344c
ohboy <singhchauhananmol500@gmail.com>
2021-09-17 10:47:49 +0530

    inital function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Icon from "./components/Icon";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Card, CardBody, Container, Button, Col, Row } from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./style.scss";
+const itemArray = new Array(9).fill("empty");
 
-function App() {
+const App = () => {
+  const [isCross, setIsCross] = useState(false);
+  const [isWinMessage, setIsWinMessage] = useState("");
+
+  const reloadGame = () => {
+    //
+  };
+  const checkIsWinner = () => {
+    //
+  };
+
+  const changeItem = (itemNumber) => {
+    //
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="p-5 ">
+      <ToastContainer position="bottom-center" />
+      <Row>
+        <Col md={6} className="offset-md-3">
+          <div className="grid">
+            {itemArray.map((item, index) => (
+              <Card>
+                <CardBody className="box">
+                  <Icon name={item} />
+                </CardBody>
+              </Card>
+            ))}
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
-}
+};
 
 export default App;
